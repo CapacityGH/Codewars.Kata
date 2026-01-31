dabd27b761ae29058169c6b2837154b8f108db3b
Capacity <lolzhenya10@gmail.com>
2026-01-31 23:36:28 +0200

    Added CountingDuplicates.cs (6-kyu)

---
+++ csharp/6-kyu/CountingDuplicates.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int DuplicateCount(string str)
+    {
+        return str.ToLower().GroupBy(c => c).Count(c => c.Count() > 1);
+    }
+}
